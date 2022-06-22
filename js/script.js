a412bf083995aaf9cb1d24ee4723ba73c410b6e3
SamueleLettieri <samuele2073@gmail.com>
2022-06-22 18:10:13 +0200

    giogo del pari e dispari incompleto

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const parolaUtente = prompt("inserisci una parola")
+/* const parolaUtente = prompt("inserisci una parola")
 console.log(parolaUtente)
 
 function parolaAlContrario (parola) {
@@ -15,6 +15,36 @@ if(parolaUtente === nuovaParolaAlContrario){
     alert(`${parolaUtente} è una parola palidroma`)
 } else {
     alert(`${parolaUtente} non è una parola palidroma`)
+} */
+
+const oddOrEven = ["pari", "dispari" ];
+
+const userOddOrEven = prompt("inserisci cosa vuoi scegliere (pari o dispari)");
+console.log(userOddOrEven);
+
+const usernumber = parseInt(prompt("inserisci un numero da 1 a 5"));
+console.log(usernumber);
+ 
+let pari;
+let dispari;
+
+if(oddOrEven[0] === userOddOrEven){
+    pari = userOddOrEven;
+    console.log(pari);
+} else if (oddOrEven[1] === userOddOrEven){
+    dispari = userOddOrEven;
+    console.log(dispari);
 }
 
- 
+
+function ia (){
+    return iaDice = parseInt(Math.random() * 5)+1;
+}
+
+let risultato = ia ();
+console.log(risultato);
+
+
+let somma = risultato + usernumber;
+console.log(somma)
+
