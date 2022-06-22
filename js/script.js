344bb76d1fd6b819614cad2dbe53e3408203f82c
SamueleLettieri <samuele2073@gmail.com>
2022-06-22 15:59:36 +0200

    programma per capire se una parola e palidroma

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const parolaUtente = prompt("inserisci una parola")
+console.log(parolaUtente)
+
+function parolaAlContrario (parola) {
+    let nuovaParola = "";
+    for (let i = parola.length - 1; i >= 0; i--) {
+        nuovaParola += parola[i];
+    }
+    return nuovaParola;
+}
+const nuovaParolaAlContrario = parolaAlContrario (parolaUtente);
+console.log(nuovaParolaAlContrario)
+
+if(parolaUtente === nuovaParolaAlContrario){
+    alert(`${parolaUtente} è una parola palidroma`)
+} else {
+    alert(`${parolaUtente} non è una parola palidroma`)
+}
+
